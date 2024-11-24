ff6b0e79295f0afd94e323d9fb78a4c1208630bf
Ishtiaque Ahmed CSEH Rafin <94455536+CsehRafin@users.noreply.github.com>
2024-11-24 14:47:14 +0600

    Create env.py

---
+++ env.py
@@ -0,0 +1,17 @@
+api_key = ""
+
+db_key = ""
+db_data = ""
+
+account_id = ""
+
+class Logic:
+    def __init__(self):
+        self.l1 = ""
+        self.l2 = ""
+        self.q_l1 = ""
+        self.q_l2 = ""
+        self.q_f1 = ""
+        self.q_f2 = ""
+
+logic = Logic()
